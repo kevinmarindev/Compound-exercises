61c3af31f598fa97cee711b0982e4897a9219b69
kevinmarindev <kevin.marin7@hotmail.com>
2022-06-05 23:54:22 -0500

    Table for seeing resutls

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,25 +6,79 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useEffect, useState } from 'react'
+import { Delete } from '@mui/icons-material'
 
-function createData(
-  date: Date | string ,
-  weight: number,
-  reps: number,
-  sets: number,
-) {
-  return { date, weight, reps, sets};
-}
+// function createData(
+//   date: Date | string ,
+//   weight?: number,
+//   reps?: number,
+//   sets?: number,
+// ) {
+//   return { date, weight, reps, sets};
+// }
+// function createData(
+//   date: Date | string,
+//   weight?: number,
+//   reps?: number,
+//   sets?: number,
+// ) {
+//   return { date, weight, reps, sets};
+// }
+// let arrays = JSON.parse(localStorage.getItem(`${window.location.pathname}`) as string) || null
+// console.log(arrays)
+// function createData(arrays : string){
+ 
+  // console.log(rows)
+  // conversionArr.
+// }
 
-const rows = [
-  createData(new Date(), 159, 6.0, 24),
-  createData(new Date(Date.now()), 237, 9.0, 37),
-  createData('Eclair', 262, 16.0, 24),
-  createData('Cupcake', 305, 3.7, 67),
-  createData('Gingerbread', 356, 16.0, 49),
-];
+
+// if(!localStorage.getItem(`${wideLocation}`)){
+//            localStorage.setItem(`${}`, JSON.stringify([item]))
+//        }
+
+interface ITableProps {
+    setDisplay?: string,
+} 
+
+
+// const rows = [
+//   createData(new Date(), 159, 6.0, 24),
+//   createData(new Date(Date.now()), 237, 9.0, 37),
+//   createData('Eclair', 262, 16.0, 24),
+//   createData('Cupcake', 305, 3.7, 67),
+//   createData('Gingerbread', 356, 16.0, 49),
+// ];
+// const rows = [
+//   createData(localStorage.getItem(`${window.location.pathname}`) as string),
+// ];
 // &nbsp;
-export default function BasicTable() {
+export default function BasicTable({ setDisplay }: ITableProps) {
+
+  const [arrayOne, setArrayOne] = useState<[][]>([])
+  const [refresh, setRefresh] = useState(0)
+  
+  useEffect(()=> {
+    // setDisplay(displayIt as string)
+    setArrayOne(() => JSON.parse(localStorage.getItem(`${window.location.pathname}`)as string )?.sort((a : any, b : any) => (new Date(a[0])as any) as any - (new Date(b[0]) as any) as any)
+    )
+    
+    
+  },[setDisplay])
+
+  const runitUp = (array: [][], item : []) => {
+    localStorage.setItem(`${window.location.pathname}`, JSON.stringify(array.filter((item2) => item2 !== item)))
+
+    setArrayOne(() => JSON.parse(localStorage.getItem(`${window.location.pathname}`)as string )?.sort((a : any, b : any) => (new Date(a[0])as any) as any - (new Date(b[0]) as any) as any)
+    )
+
+  }
+
+ 
+  
+  console.log('rendering table ')
+  
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -37,18 +91,23 @@ export default function BasicTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {arrayOne?.map((item : any, idx: number, array) => (
             <TableRow
-              key={row.sets}
+              key={Math.random() * 100}
               sx={{ '&:last-child td, &:last-child th': { border: 1 } }}
+              // onClick={() => setArrayOne(array.filter((thing) => thing !== item)}
+              // onClick={()=> localStorage.setItem(`${window.location.pathname}`, JSON.stringify(array.filter((item2) => item2 !== item)))}
+              // onMouseUp={()=> setRefresh((prev)=> prev + 1)}
+              onClick={() => runitUp(array, item)}
+             
             >
               <TableCell component="th" scope="row">
-                {row.date.toLocaleString()}
+                {String(item[0])}
               </TableCell>
-              <TableCell align="right">{row.weight}</TableCell>
-              <TableCell align="right">{row.reps}</TableCell>
-              <TableCell align="right">{row.sets}</TableCell>
-              {/* <TableCell align="right">{row.protein}</TableCell> */}
+              <TableCell align="right">{item[1]}</TableCell>
+              <TableCell align="right">{item[2]}</TableCell>
+              <TableCell align="right" >{item[3]}</TableCell>
+              <TableCell align="right" ><Delete/></TableCell>
             </TableRow>
           ))}
         </TableBody>
