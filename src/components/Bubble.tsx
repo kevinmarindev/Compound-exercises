88b1cfb65e326e254310fe133dcd7942919ddd4b
kevinmarindev <kevin.marin7@hotmail.com>
2022-06-07 01:25:38 -0500

    chart component created

---
+++ src/components/Bubble.tsx
@@ -0,0 +1,10 @@
+import { Bubble } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+
+const Bubbles = () => {
+  return (
+    <div>Buble place</div>
+  )
+}
+
+export default Bubbles
