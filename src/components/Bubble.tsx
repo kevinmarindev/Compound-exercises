23902027bb56ccc74f5551072938835023e9537e
kevinmarindev <kevin.marin7@hotmail.com>
2022-06-08 01:50:20 -0500

    changes made to options

--- src/components/Bubble.tsx
+++ src/components/Bubble.tsx
@@ -2,6 +2,12 @@ import { Bubble } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 const Bubbles = () => {
+    const options = {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },};
   return (
     <div>Buble place</div>
   )
