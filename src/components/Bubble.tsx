c7f6e6b040f1b7d353eac70428a6fe49d364f7ab
kevinmarindev <kevin.marin7@hotmail.com>
2022-06-16 23:22:26 -0500

    trynna figure out how to make it look better

--- src/components/Bubble.tsx
+++ src/components/Bubble.tsx
@@ -14,9 +14,6 @@ const options = {
             y: {
                 beginAtZero: true,
             },
-            x: {
-              beginAtZero: true,
-            }
         },};
 
 interface ITableProps {
